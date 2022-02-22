58508d6af257978e813a506c8f015466e02eb0e2
wuchengji <17601056863@163.com>
2022-02-22 10:30:04 +0800

    feat: 1.1 创建canvas元素步骤

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log(1312);
+window.onload = () => {
+  const canvas = document.querySelector<HTMLCanvasElement>("#canvas") || new HTMLCanvasElement();
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  console.log(canvas.toDataURL());
+}
+
