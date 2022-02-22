b9eb5aaf1baf981344f0c428753be25c8b38dce6
wuchengji <17601056863@163.com>
2022-02-22 09:49:08 +0800

    init：初始化

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+    compress: true,
+    port: 6688,
+    open: false,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', '.js' ]
+  },
+  output: {
+    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      hash: true
+    })
+  ],
+  mode: 'development'
+};
